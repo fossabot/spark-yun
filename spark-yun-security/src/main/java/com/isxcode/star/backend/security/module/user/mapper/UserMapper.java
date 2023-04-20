4f361e472489e9b9c47afa15d6e39d98bded2c26
ispong <ispong@outlook.com>
2023-04-20 12:30:39 +0800

    :sparkles: jpa支持记录创建人，更新人等信息

--- spark-yun-security/src/main/java/com/isxcode/star/backend/security/module/user/mapper/UserMapper.java
+++ spark-yun-security/src/main/java/com/isxcode/star/backend/security/module/user/mapper/UserMapper.java
@@ -1,7 +1,26 @@
 package com.isxcode.star.backend.security.module.user.mapper;
 
+import com.isxcode.star.api.pojos.tenant.req.TetAddUserReq;
+import com.isxcode.star.api.pojos.tenant.req.TetUpdateUserReq;
+import com.isxcode.star.backend.security.module.user.entity.UserEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 /** mapstruct映射. */
 @Mapper(componentModel = "spring")
-public interface UserMapper {}
+public interface UserMapper {
+
+  @Mapping(target = "passwd", source = "password")
+  @Mapping(target = "commentInfo", source = "comment")
+  UserEntity tetAddUserReqToUserEntity(TetAddUserReq tetAddUserReq);
+
+  @Mapping(target = "passwd", source = "tetUpdateUserReq.password")
+  @Mapping(target = "commentInfo", source = "tetUpdateUserReq.comment")
+  @Mapping(target = "account", source = "tetUpdateUserReq.account")
+  @Mapping(target = "id", source = "userEntity.id")
+  @Mapping(target = "username", source = "tetUpdateUserReq.username")
+  @Mapping(target = "phone", source = "tetUpdateUserReq.phone")
+  @Mapping(target = "email", source = "tetUpdateUserReq.email")
+  UserEntity tetUpdateUserReqToUserEntity(TetUpdateUserReq tetUpdateUserReq, UserEntity userEntity);
+
+}
